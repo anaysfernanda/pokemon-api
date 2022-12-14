c5a36f4ef8eda44c552adc1bd4c805806effff0f
Fernanda de Souza <a.fernandadesouza@gmail.com>
2022-12-13 22:15:23 -0300

    segundo commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,28 @@
 import {
   Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
   Grid,
-  IconButton,
   Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   TextField,
   Typography,
 } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { selectTasks, taskAdd, tasksGetAll } from "../store/modules/TaskSlice";
+import { pokemonGetOne } from "../store/modules/PokemonSlice";
 
 const Home: React.FC = () => {
-  const tasksRedux = useAppSelector(selectTasks);
+  const [nome, setNome] = useState<string>("");
+
+  const pokemonRedux = useAppSelector((state) => state.pokemon);
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(tasksGetAll());
-  }, []);
-
-  // const handleAddTask = () => {
-  //   dispatch(taskAdd());
-  // };
+  const handleGetPokemon = () => {
+    console.log(pokemonRedux.species);
+    dispatch(pokemonGetOne(nome));
+  };
 
   return (
     <Grid
@@ -57,59 +51,39 @@ const Home: React.FC = () => {
           <Grid item xs={9}>
             <TextField
               id="outlined-basic"
-              label="Nome da tarefa"
+              label="Digite o Pokemon"
               variant="outlined"
               sx={{ mb: 1 }}
               fullWidth
+              value={nome}
+              onChange={(ev) => setNome(ev.target.value)}
             ></TextField>
-            <TextField
-              id="outlined-basic"
-              label="Descrição da tarefa"
-              variant="outlined"
-              fullWidth
-            ></TextField>
+
             <Button
               variant="contained"
               sx={{ m: 2, backgroundColor: "#818181" }}
+              onClick={handleGetPokemon}
             >
-              Adicionar
+              Buscar
             </Button>
-            <TableContainer component={Paper} sx={{ mb: 3 }}>
-              <Table sx={{ minWidth: "300px" }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">Tarefa</TableCell>
-                    <TableCell align="center">Descrição</TableCell>
-                    <TableCell align="center">Ação</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {tasksRedux.map((row) => (
-                    <TableRow
-                      key={row.title}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell align="center">{row.title}</TableCell>
-                      <TableCell align="center">{row.description}</TableCell>
-                      <TableCell align="center">
-                        <>
-                          <IconButton
-                            edge="end"
-                            aria-label="edit"
-                            sx={{ marginRight: "10px" }}
-                          >
-                            <EditIcon />
-                          </IconButton>
-                          <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                          </IconButton>
-                        </>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image=""
+                  alt="pokemon"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {pokemonRedux.name}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {pokemonRedux.height}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           </Grid>
         </Paper>
       </Grid>
--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import tasks from "./TaskSlice";
+import pokemon from "./PokemonSlice";
 
 export default combineReducers({
   tasks,
+  pokemon,
 });
