1c819c073165b88320a934855f6748e17dce1600
Darlan Fabricio <darlanfabricio@gmail.com>
2022-12-15 19:33:05 -0300

    quinto commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,13 +10,18 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../store/hooks";
 import { pokemonGetOne } from "../store/modules/PokemonSlice";
 
 const Home: React.FC = () => {
   const [nome, setNome] = useState<string>("");
 
-  const pokemonRedux = useAppSelector((state) => state.pokemon);
+  const pokemonRedux = useAppSelector(
+    (state) => state.pokemon
+  );
   const dispatch = useAppDispatch();
 
   const handleGetPokemon = () => {
@@ -44,7 +49,10 @@ const Home: React.FC = () => {
             alignItems: "center",
           }}
         >
-          <Typography variant="h4" sx={{ textAlign: "center", m: 2 }}>
+          <Typography
+            variant="h4"
+            sx={{ textAlign: "center", m: 2 }}
+          >
             Adicione uma tarefa
           </Typography>
           <Grid item xs={9}>
@@ -55,7 +63,9 @@ const Home: React.FC = () => {
               sx={{ mb: 1 }}
               fullWidth
               value={nome}
-              onChange={(ev) => setNome(ev.target.value)}
+              onChange={(ev) =>
+                setNome(ev.target.value)
+              }
             ></TextField>
 
             <Button
@@ -68,21 +78,37 @@ const Home: React.FC = () => {
             >
               BuscarC
             </Button>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={pokemonRedux?.sprites?.front_default}
-                  alt="pokemon"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {pokemonRedux?.name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            {pokemonRedux?.name && (
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={
+                      pokemonRedux?.sprites
+                        ?.front_default
+                    }
+                    alt="pokemon"
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                    >
+                      {pokemonRedux?.name}
+                    </Typography>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                    >
+                      {pokemonRedux?.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            )}
           </Grid>
         </Paper>
       </Grid>
